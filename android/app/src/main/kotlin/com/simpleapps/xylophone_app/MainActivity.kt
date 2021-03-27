4df753027f4a38437ba98f94f594001b25664386
Дмитрий Маркин <dima-markin-2000@yandex.ru>
2021-03-27 13:57:51 +0300

    Application created. Fixed issue, when MediaPlayerNative got some trouble after a while.

---
+++ android/app/src/main/kotlin/com/simpleapps/xylophone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.simpleapps.xylophone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: XylophonePage(),
+  ));
+}
+
+class XylophonePage extends StatefulWidget {
+  @override
+  _XylophonePageState createState() => _XylophonePageState();
+  final _player = AudioCache()..fixedPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
+}
+
+class _XylophonePageState extends State<XylophonePage> {
+  Widget _createMusicLine({Color color: Colors.red, int soundNumber}) =>
+      Expanded(
+        child: MaterialButton(
+          color: color,
+          onPressed: () {
+            widget._player.play('note$soundNumber.wav');
+          }
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _createMusicLine(color: Colors.red, soundNumber: 1),
+            _createMusicLine(color: Colors.orange, soundNumber: 2),
+            _createMusicLine(color: Colors.yellow, soundNumber: 3),
+            _createMusicLine(color: Colors.green, soundNumber: 4),
+            _createMusicLine(color: Colors.teal, soundNumber: 5),
+            _createMusicLine(color: Colors.blue, soundNumber: 6),
+            _createMusicLine(color: Colors.purple, soundNumber: 7),
+          ],
+        ),
+      ),
+    );
+  }
+}
